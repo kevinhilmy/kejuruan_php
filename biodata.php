43c4ad83d8d9ccd9a94d30de7207b64899c83add
kevinhilmy <kevinhilmy.r@gmail.com>
2025-02-08 17:19:05 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class= "biodata">
+        <div class = "judul">
+            <h1>Biodata Kelompok 3</h1>
+        </div>
+        <div class = "isi">
+            <?php
+            echo "
+            Nama : Kevin Hilmy Ramadhan <br>
+            Umur : 16 Tahun <br>
+            Alamat : kiara Condong <br> <br>";
+
+            echo "
+            Nama : Hisyam Fathir Alfariz <br>
+            Umur : 15 Tahun <br>
+            Alamat : Ciumbuleuit <br> <br>";
+            
+            echo "
+            Nama : Ipah Rahmah Syamilah <br>
+            Umur : 16 Tahun <br>
+            Alamat : Sekeloa Utara <br> <br>";
+            
+            echo "
+            Nama : Iqbal Pratama <br>
+            Umur : 16 Tahun <br>
+            Alamat : Cijotang <br> <br>";
+
+            echo "
+            Nama : Keysha Naurah <br>
+            Umur : 16 Tahun <br>
+            Alamat : Cicadas <br> <br>";
+            
+            echo "
+            Nama : Mochammad Reyhan Arrofi <br>
+            Umur : 16 Tahun <br>
+            Alamat : Sukaluyu <br> <br>";
+            ?>
+    </div>
+</div>
+</body>
+</html>
---
+++ biodata2.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="biodata">
+        <div class="judul">
+            <h1>Biodata Kelompok 3</h1>
+        </div>
+        <div class="isi">
+            <?php
+                $Nama1= "Keysha Naurah";
+                $Umur1= 16;
+                $Alamat1= "Cicadas";
+                $Hobby1= "Menyanyi";
+
+                $Nama2= "Ipah Rahmah";
+                $Umur2= 16;
+                $Alamat2= "Sekeloa Utara";
+                $Hobby2= "Hiking";
+
+                $Nama3= "Kevin Hilmy Ramadhan";
+                $Umur3= 16;
+                $Alamat3= "Kiara Condong";
+                $Hobby3= "Main Basket";
+
+                $Nama4= "Mochamad Reyhan Arrofi";
+                $Umur4= 115;
+                $Alamat4= "Sukaluyu";
+                $Hobby4= "Beli Mobil";
+
+                $Nama5= "Iqbal Pratama";
+                $Umur5= 16;
+                $Alamat5= "Cijotang";
+                $Hobby5= "Futsal";
+
+                $Nama6= "Hisyam Fathir Alfariz";
+                $Umur6= 15;
+                $Alamat6= "Cimbuleuit";
+                $Hobby6= "Baca Buku";
+            ?>
+
+            <div class = "kolom">
+                <?php
+                    echo "<p><strong>Nama:</strong> $Nama1</p>";
+                    echo "<p><strong>Umur:</strong> $Umur1</p>";
+                    echo "<p><strong>Alamat:</strong> $Alamat1</p>";
+                    echo "<p><strong>Hobby:</strong> $Hobby1</p><br>";
+                    
+                    echo "<p><strong>Nama:</strong> $Nama2</p>";
+                    echo "<p><strong>Umur:</strong> $Umur2</p>";
+                    echo "<p><strong>Alamat:</strong> $Alamat2</p>";
+                    echo "<p><strong>Hobby:</strong> $Hobby2</p><br>";
+                    
+                    echo "<p><strong>Nama:</strong> $Nama3</p>";
+                    echo "<p><strong>Umur:</strong> $Umur3</p>";
+                    echo "<p><strong>Alamat:</strong> $Alamat3</p>";
+                    echo "<p><strong>Hobby:</strong> $Hobby3</p><br>";
+                ?>
+            </div>
+
+            <div class = "kolom">
+                <?php
+                    echo "<p><strong>Nama:</strong> $Nama4</p>";
+                    echo "<p><strong>Umur:</strong> $Umur4</p>";
+                    echo "<p><strong>Alamat:</strong> $Alamat4</p>";
+                    echo "<p><strong>Hobby:</strong> $Hobby4</p><br>";
+                    
+                    echo "<p><strong>Nama:</strong> $Nama5</p>";
+                    echo "<p><strong>Umur:</strong> $Umur5</p>";
+                    echo "<p><strong>Alamat:</strong> $Alamat5</p>";
+                    echo "<p><strong>Hobby:</strong> $Hobby5</p><br>";
+                    
+                    echo "<p><strong>Nama:</strong> $Nama6</p>";
+                    echo "<p><strong>Umur:</strong> $Umur6</p>";
+                    echo "<p><strong>Alamat:</strong> $Alamat6</p>";
+                    echo "<p><strong>Hobby:</strong> $Hobby6</p><br>";
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
